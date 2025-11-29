8ee66d2230d8fabfa0a3e1f4ab47fc1767480452
8F76 <2466662468@qq.com>
2025-11-30 05:19:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
 
   AUTHOR: 'Lyrics', // 您的昵称 例如 tangly1024
   BIO: 'NEFU CS Sophomore | Acmer', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-nu-ten-44.vercel.app/', // 网站地址
+  LINK: 'https://notion-next-three-psi-33.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
